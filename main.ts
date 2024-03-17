1d0bff454d87a426589c5d9ea250bc6b3d545b81
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-17 21:32:13 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+let summer = true;
+if (summer) {
+    console.log("Stay at Home.");
+}
+else {
+    console.log("Drink a lot of water.");
+}
+
+let careerGoal = true
+if (careerGoal) {
+    console.log("Focus on your Career because at the end people don't see your heart they see your success.");
+}
+else {
+    console.log("Set your Career goals high and don't stop till you get there.");
+}
+
+let isStuding = false
+if (isStuding) {
+    console.log("Work hard to get good result.");
+}
+else {
+    console.log("Your result doesn't decide Where you stand in future.")
+}
